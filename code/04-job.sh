ea10644b0180340cd9c28a80d166dc5c9e4a7038
Zach Bengtsson <91245841+zbengt@users.noreply.github.com>
2023-04-21 17:00:55 -0700

    Update 04-job.sh

--- code/04-job.sh
+++ code/04-job.sh
@@ -12,9 +12,9 @@
 ## Memory per node
 #SBATCH --mem=100G
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=sr320@uw.edu
+#SBATCH --mail-user=zbengt@uw.edu
 ## Specify the working directory for this job
-#SBATCH --chdir=/gscratch/scrubbed/sr320/assign_04
+#SBATCH --chdir=/gscratch/scrubbed/zbengt/assign_04
 
 #run a python script you wrote!
 module load intel-python3_2017
